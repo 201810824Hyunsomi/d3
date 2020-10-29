6657f73bc666d578141952e2b9aea62a26abcca8
201810824Hyunsomi <62291578+201810824Hyunsomi@users.noreply.github.com>
2020-10-30 00:54:46 +0900

    Add files via upload

--- src/map/Map.js
+++ src/map/Map.js
@@ -7,15 +7,93 @@ export default function Map() {
     mapscript();
   }, []);
 
+  
+
+    
   const mapscript = () => {
     let container = document.getElementById("map");
     let options = {
       center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
       level: 4,
     };
-
+    
     //map
     const map = new kakao.maps.Map(container, options);
+    
+    //줌 컨트롤 생성
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+    // 지도가 확대 또는 축소되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, 'zoom_changed', function() {        
+    
+      // 지도의 현재 레벨을 얻어옵니다
+      var level = map.getLevel();
+      var resultDiv = document.getElementById('result');  
+      
+  });
+    // 커스텀 오버레이가 표시될 위치입니다 
+    var position = new kakao.maps.LatLng(37.624915253753194, 127.15122688059974);
+    // 커스텀 오버레이에 표시할 내용입니다
+    // HTML 문자열 또는 Dom Element 입니다
+    var content = '<div class="overlay_info">';
+    content += '    <a href="https://place.map.kakao.com/23279610" target="_blank"><strong>수유리 우동집</strong></a>';
+    content += '    <div class="desc">';
+    content += '        <span class="address">경기 남양주시 늘을2로 26 (우)12150</span>';
+    content += '    </div>';
+    content += '</div>';
+    // 커스텀 오버레이를 생성합니다
+    var mapCustomOverlay = new kakao.maps.CustomOverlay({
+      position: position,
+      content: content,
+      xAnchor: 0.5, // 커스텀 오버레이의 x축 위치입니다. 1에 가까울수록 왼쪽에 위치합니다. 기본값은 0.5 입니다
+      yAnchor: 1.1 // 커스텀 오버레이의 y축 위치입니다. 1에 가까울수록 위쪽에 위치합니다. 기본값은 0.5 입니다
+    });
+
+// 커스텀 오버레이를 지도에 표시합니다
+mapCustomOverlay.setMap(map);
+
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places(); 
+
+// 키워드로 장소를 검색합니다
+ps.keywordSearch('한전', placesSearchCB); 
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }       
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
+}
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x) 
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
+}
     markerdata.forEach((el) => {
       // 마커를 생성합니다
       const marker = new kakao.maps.Marker({
@@ -23,49 +101,28 @@ export default function Map() {
         map: map,
         //마커가 표시 될 위치
         position: new kakao.maps.LatLng(el.lat, el.lng),
+        
       });
       // 마커에 표시할 인포윈도우를 생성합니다
       var infowindow = new kakao.maps.InfoWindow({
         content: el.title, // 인포윈도우에 표시할 내용
+        content: el.id,
       });
-      
-      infowindow.open(map,marker)
-      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-      // 이벤트 리스너로는 클로저를 만들어 등록합니다
-      // 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-      // kakao.maps.event.addListener(
-      //   marker,
-      //   "mouseover",
-      //   makeOverListener(map, marker, infowindow)
-      // );
-      // kakao.maps.event.addListener(
-      //   marker,
-      //   "mouseout",
-      //   makeOutListener(infowindow)
-      // );
-      // kakao.maps.event.addListener(
-      //   marker,
-      //   makeOver(map, marker, infowindow)
-      // );
+      // 중심옮기기
+      // map.setCenter(position);
+      kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커 위에 인포윈도우를 표시합니다
+        infowindow.open(map, marker);  
+        
+  });
+      kakao.maps.event.addListener(marker, 'rightclick', function() {
+      // 마커 위에 인포윈도우를 표시합니다
+      infowindow.close();  
+    
+});
+    
     });
-    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-    function makeOverListener(map, marker, infowindow) {
-      return function () {
-        infowindow.open(map, marker);
-      };
-    } 
-    // 자동으로 인포윈도우를 출력하는 함수입니다.
-    function makeOver(map, marker, infowindow) {
-      return function () {
-        infowindow.open(map, marker);
-      };
-    }
-    // 인포윈도우를 닫는 클로저를 만드는 함수입니다
-    function makeOutListener(infowindow) {
-      return function () {
-        infowindow.close();
-      };
-    }
+      
   };
 
   return <div id="map" style={{ width: "100vw", height: "100vh" }}></div>;
